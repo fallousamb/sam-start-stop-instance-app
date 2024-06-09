6c88d7e9aec9e4e669ddbc9173d19f450da59d47
fallousamb <fallousamb74@gmail.com>
2024-06-09 17:48:11 +0200

    Reformat code

--- instances-management/src/main/java/mouride/dev/aws/lambda/StartInstance.java
+++ instances-management/src/main/java/mouride/dev/aws/lambda/StartInstance.java
@@ -8,13 +8,50 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import java.util.Map;
 
+/**
+ * This class represents a Lambda function to start an EC2 instance.
+ * It uses the AWS SDK for Java (v2) to interact with the EC2 service.
+ */
 public class StartInstance implements RequestHandler<Map<String, String>, String> {
+
+    private final AmazonEC2 ec2;
+
+    private final String instanceId;
+
+    /**
+     * Default constructor.
+     * It initializes the AmazonEC2 client using the default credentials provider chain.
+     * It retrieves the instance ID from the environment variable "INSTANCE_ID".
+     */
+    public StartInstance() {
+        this.ec2 = AmazonEC2ClientBuilder.defaultClient();
+        this.instanceId = System.getenv("INSTANCE_ID");
+    }
+
+    /**
+     * Constructor for dependency injection (useful for testing).
+     *
+     * @param ec2       The AmazonEC2 client to use for making API calls.
+     * @param instanceId The ID of the EC2 instance to start.
+     */
+    public StartInstance(AmazonEC2 ec2, String instanceId) {
+        this.ec2 = ec2;
+        this.instanceId = instanceId;
+    }
+
+    /**
+     * This method is called when the Lambda function is triggered.
+     * It starts the specified EC2 instance and returns a confirmation message.
+     *
+     * @param input   The input to the Lambda function. In this case, it is a map of string to string.
+     * @param context The context in which the Lambda function is running.
+     * @return A confirmation message indicating that the instance has been started.
+     */
     @Override
     public String handleRequest(Map<String, String> input, Context context) {
-        final String instanceId = System.getenv("INSTANCE_ID");
-        AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
         StartInstancesRequest request = new StartInstancesRequest().withInstanceIds(instanceId);
         ec2.startInstances(request);
-        return "Instance started";
+        return String.format("Instance %s started", instanceId);
     }
+
 }
--- instances-management/src/main/java/mouride/dev/aws/lambda/StopInstance.java
+++ instances-management/src/main/java/mouride/dev/aws/lambda/StopInstance.java
@@ -8,13 +8,54 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import java.util.Map;
 
+/**
+ * This class represents a Lambda function to stop an EC2 instance.
+ * It can be initialized with an existing AmazonEC2 client or with default client and instanceId from environment variables.
+ */
 public class StopInstance implements RequestHandler<Map<String, String>, String> {
+
+    /**
+     * The AmazonEC2 client used to interact with EC2 service.
+     */
+    private final AmazonEC2 ec2;
+
+
+    /**
+     * The ID of the EC2 instance to stop.
+     */
+    private final String instanceId;
+
+    /**
+     * Constructor to initialize with an existing AmazonEC2 client and instanceId.
+     *
+     * @param ec2       The AmazonEC2 client.
+     * @param instanceId The ID of the EC2 instance to stop.
+     */
+    public StopInstance(AmazonEC2 ec2, String instanceId) {
+        this.ec2 = ec2;
+        this.instanceId = instanceId;
+    }
+
+    /**
+     * Constructor to initialize with default AmazonEC2 client and instanceId from environment variables.
+     */
+    public StopInstance() {
+        this.ec2 = AmazonEC2ClientBuilder.defaultClient();
+        this.instanceId = System.getenv("INSTANCE_ID");
+    }
+
+    /**
+     * The handleRequest method is the entry point for the Lambda function.
+     * It stops the specified EC2 instance and returns a confirmation message.
+     *
+     * @param input   The input to the Lambda function. In this case, it's a map of string to string.
+     * @param context The context of the Lambda function.
+     * @return A confirmation message indicating that the instance has been stopped.
+     */
     @Override
     public String handleRequest(Map<String, String> input, Context context) {
-        final String instanceId = System.getenv("INSTANCE_ID");
-        AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
         StopInstancesRequest request = new StopInstancesRequest().withInstanceIds(instanceId);
         ec2.stopInstances(request);
-        return "Instance stopped";
+        return String.format("Instance %s stopped", instanceId);
     }
 }
