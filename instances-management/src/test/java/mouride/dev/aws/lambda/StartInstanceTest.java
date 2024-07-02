a8c76e1c57ad9c469a7f2d42f1c4c8cb4f8f5dc0
fallousamb <fallousamb74@gmail.com>
2024-07-02 17:54:49 +0200

    Add TU to lambda function

--- instances-management/src/test/java/mouride/dev/aws/lambda/StartInstanceTest.java
+++ instances-management/src/test/java/mouride/dev/aws/lambda/StartInstanceTest.java
@@ -3,9 +3,12 @@ package mouride.dev.aws.lambda;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.model.StartInstancesRequest;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -14,36 +17,90 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
- class StartInstanceTest {
+class StartInstanceTest {
 
     @Mock
     private AmazonEC2 ec2;
 
+    @Mock
+    private LambdaLogger logger;
+
     @InjectMocks
     private StartInstance startInstance;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @BeforeEach
     void setUp() {
-        String instanceId = "i-1234567890abcdef0";
-        startInstance = new StartInstance(ec2, instanceId);
+        System.setProperty("INSTANCE_ID", "i-0a4779856f887e215");
+        startInstance = new StartInstance(ec2, objectMapper, System.getProperty("INSTANCE_ID"));
     }
 
     @Test
-    void testHandleRequest() {
+    void testHandleRequestStartSuccess() {
         // Given
-        Map<String, String> event = new HashMap<>();
-        event.put("InstanceId", "i-1234567890abcdef0");
-        Context context = mock(Context.class);
+        // Create test event
+        final Map<String, Object> detail = new HashMap<>();
+        detail.put("action", "start");
+        final Map<String, Object> event = new HashMap<>();
+        event.put("detail", detail);
+
+        final Context context = mock(Context.class);
 
         // Act
-        String response = startInstance.handleRequest(event, context);
+        final String response = startInstance.handleRequest(event, context);
 
-        // Then
-        verify(ec2, times(1)).startInstances(any(StartInstancesRequest.class));
-        assertEquals(String.format("Instance %s started", event.get("InstanceId")), response);
+        // Capture the request sent to AWS EC2 client
+        final ArgumentCaptor<StartInstancesRequest> requestCaptor = ArgumentCaptor.forClass(StartInstancesRequest.class);
+        verify(ec2, times(1)).startInstances(requestCaptor.capture());
+        final StartInstancesRequest startRequest = requestCaptor.getValue();
+
+        // Assert the request details
+        assertEquals("i-0a4779856f887e215", startRequest.getInstanceIds().get(0));
+        assertEquals("Instance i-0a4779856f887e215 started successfully", response);
+    }
+
+    @Test
+    void testHandleRequest_UnknownAction() {
+        // Create test event
+        final Map<String, Object> detail = new HashMap<>();
+        detail.put("action", "unknown");
+        final Map<String, Object> event = new HashMap<>();
+        event.put("detail", detail);
+        final Context context = mock(Context.class);
+
+        // Call the handler
+        final String result = startInstance.handleRequest(event, context);
+
+        // Assert the response
+        assertEquals(String.format("Received event for action '%s', but only start action is supported", detail.get("action")),
+                result);
+
+        // Verify no interaction with EC2 client
+        verifyNoInteractions(ec2);
+    }
+
+    @Test
+    void testHandleRequestErrorStartingInstance() {
+        // Create test event
+        final Map<String, Object> detail = new HashMap<>();
+        final Map<String, Object> event = new HashMap<>();
+        event.put("detail", detail);
+        final Context context = mock(Context.class);
+
+        // When
+        when(context.getLogger()).thenReturn(logger);
+
+        // Call the handler
+        final String result = startInstance.handleRequest(event, context);
+
+        // Assert the response
+        assertEquals(String.format("Failed to start instance %s", System.getProperty("INSTANCE_ID")), result);
+
+        // Verify no interaction with EC2 client
+        verifyNoInteractions(ec2);
     }
 }
--- instances-management/src/test/java/mouride/dev/aws/lambda/StopInstanceTest.java
+++ instances-management/src/test/java/mouride/dev/aws/lambda/StopInstanceTest.java
@@ -1,12 +1,14 @@
 package mouride.dev.aws.lambda;
 
 import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.model.StartInstancesRequest;
 import com.amazonaws.services.ec2.model.StopInstancesRequest;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -24,25 +26,81 @@ class StopInstanceTest {
     @Mock
     private AmazonEC2 ec2;
 
+    @Mock
+    private LambdaLogger logger;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @InjectMocks
     private StopInstance stopInstance;
 
     @BeforeEach
     void setUp() {
-        String instanceId = "i-1234567890abcdef0";
-        stopInstance = new StopInstance(ec2, instanceId);
+        System.setProperty("INSTANCE_ID", "i-0a4779856f887e215");
+        stopInstance = new StopInstance(ec2, objectMapper, System.getProperty("INSTANCE_ID"));
     }
 
     @Test
-    void testStopInstance() {
+    void testStopInstanceSuccess() {
         // Given
-        Map<String, String> event = new HashMap<>();
-        event.put("InstanceId", "i-1234567890abcdef0");
-        Context context = mock(Context.class);
-        String response = stopInstance.handleRequest(event, context);
+        // Create test event
+        final Map<String, Object> detail = new HashMap<>();
+        detail.put("action", "stop");
+        final Map<String, Object> event = new HashMap<>();
+        event.put("detail", detail);
+
+        final Context context = mock(Context.class);
+        final String response = stopInstance.handleRequest(event, context);
+
+        // Capture the request sent to AWS EC2 client
+        final ArgumentCaptor<StopInstancesRequest> requestCaptor = ArgumentCaptor.forClass(StopInstancesRequest.class);
+        verify(ec2, times(1)).stopInstances(requestCaptor.capture());
+        final StopInstancesRequest startRequest = requestCaptor.getValue();
 
         // Then
-        verify(ec2, times(1)).stopInstances(any(StopInstancesRequest.class));
-        assertEquals(String.format("Instance %s stopped", event.get("InstanceId")), response);
+        verify(ec2, times(1)).stopInstances(any(StopInstancesRequest.class));// Assert the request details
+        assertEquals("i-0a4779856f887e215", startRequest.getInstanceIds().get(0));
+        assertEquals("Instance i-0a4779856f887e215 stopped successfully", response);
+    }
+
+    @Test
+    void testHandleRequest_UnknownAction() {
+        // Create test event
+        final Map<String, Object> detail = new HashMap<>();
+        detail.put("action", "unknown");
+        final Map<String, Object> event = new HashMap<>();
+        event.put("detail", detail);
+        final Context context = mock(Context.class);
+
+        // Call the handler
+        final String result = stopInstance.handleRequest(event, context);
+
+        // Assert the response
+        assertEquals(String.format("Received event for action '%s', but only stop action is supported", detail.get("action")),
+                result);
+
+        // Verify no interaction with EC2 client
+        verifyNoInteractions(ec2);
+    }
+
+    @Test
+    void testHandleRequestErrorStopingingInstance() {
+        // Create test event
+        final Map<String, Object> detail = new HashMap<>();
+        final Map<String, Object> event = new HashMap<>();
+        event.put("detail", detail);
+        final Context context = mock(Context.class);
+
+        // When
+        when(context.getLogger()).thenReturn(logger);
+
+        // Call the handler
+        final String result = stopInstance.handleRequest(event, context);
+
+        // Assert the response
+        assertEquals(String.format("Failed to stop instance %s", System.getProperty("INSTANCE_ID")), result);
+
+        // Verify no interaction with EC2 client
+        verifyNoInteractions(ec2);
     }
 }
